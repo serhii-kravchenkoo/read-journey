a4960bf1db1518d5fc5f96108de406153150f3bc
Serhii Kravcnenko <sergkrava1983@gmail.com>
2026-02-20 23:33:42 +0200

    MobileMenu css

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -1,31 +1,42 @@
-import { NavLink } from "react-router-dom";
-import css from "./MobileMenu.module.css";
+import { NavLink } from 'react-router-dom';
+import css from './MobileMenu.module.css';
 
 const MobileMenu = ({ isOpen, toggleMenu, handleLogOut }) => {
   return (
     <div
-      className={`${css.overlay} ${isOpen ? css.open : ""}`}
+      className={`${css.overlay} ${isOpen ? css.open : ''}`}
       onClick={toggleMenu} // закриваємо меню при кліку на overlay
     >
       <div
-        className={`${css.mobileMenuPanel} ${isOpen ? css.openPanel : ""}`}
-        onClick={(e) => e.stopPropagation()} // запобігаємо закриттю при кліку всередині
+        className={`${css.mobileMenuPanel} ${isOpen ? css.openPanel : ''}`}
+        onClick={e => e.stopPropagation()}
       >
-        {/* Кнопка закриття */}
-        <button className={css.closeBtn} onClick={toggleMenu}>X
-          {/* <svg className={css.closeIcon} width="14" height="14">
-          <use href="/sprite.svg#icon-close-menu" />
-        </svg> */}
+        <button className={css.closeBtn} onClick={toggleMenu}>
+          <svg className={css.closeIcon} width="28" height="28">
+            <use href="/icons.svg#icon-x" />
+          </svg>
         </button>
 
-        {/* Навігація */}
         <nav className={css.nav}>
-        <NavLink to="/recommended" className={({ isActive }) => isActive ? css.active : ""} onClick={toggleMenu}>Home</NavLink>
-        <NavLink to="/library" className={({ isActive }) => isActive ? css.active : ""} onClick={toggleMenu}>My library</NavLink>
-      </nav>
+          <NavLink
+            to="/recommended"
+            className={({ isActive }) => (isActive ? css.active : css.noActive)}
+            onClick={toggleMenu}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/library"
+            className={({ isActive }) => (isActive ? css.active : css.noActive)}
+            onClick={toggleMenu}
+          >
+            My library
+          </NavLink>
+        </nav>
 
-        {/* Кнопка виходу */}
-        <button className={css.logoutBtn} onClick={handleLogOut}>Log out</button>
+        <button className={css.logoutBtn} onClick={handleLogOut}>
+          Log out
+        </button>
       </div>
     </div>
   );
