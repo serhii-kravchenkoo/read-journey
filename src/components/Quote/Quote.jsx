00ab7ad369125f4756bfa2af20ef27b547edefb0
Serhii Kravcnenko <sergkrava1983@gmail.com>
2026-02-20 22:08:34 +0200

    css library page

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -2,11 +2,12 @@ import styles from './Quote.module.css';
 
 export default function Quote() {
   return (
-    <blockquote className={styles.quote}>
-      <p>
-        "Books are windows to the world, and reading is a journey into the
-        unknown."
+    <section className={styles.quote}>
+      <img className={styles.quoteImg} src="/src/img/📚.png" alt="book" />
+      <p className={styles.quoteText}>
+        "Books are <span className={styles.quoteSpan}>windows</span> to the
+        world, and reading is a journey into the unknown."
       </p>
-    </blockquote>
+    </section>
   );
 }
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,14 @@
-import Header from "../components/Header/Header";
-import { Outlet } from "react-router-dom";
+import Header from '../components/Header/Header';
+import { Outlet } from 'react-router-dom';
+import style from './MainLayout.module.css';
 
 export default function MainLayout() {
   return (
-    <>
+    <div className={style.layout}>
       <Header />
       <main>
         <Outlet />
       </main>
-    </>
+    </div>
   );
 }
