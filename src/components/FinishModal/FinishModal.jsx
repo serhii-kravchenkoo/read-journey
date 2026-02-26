06ee398b707984aa80eb31d9a9be4fe23fb1a8ce
Serhii Kravcnenko <sergkrava1983@gmail.com>
2026-02-26 15:11:25 +0200

    finish css

--- src/components/FinishModal/FinishModal.jsx
+++ src/components/FinishModal/FinishModal.jsx
@@ -1,33 +1,45 @@
-import styles from "./FinishModal.module.css";
-import { createPortal } from "react-dom";
-import { useEffect } from "react";
+import styles from './FinishModal.module.css';
+import { createPortal } from 'react-dom';
+import { useEffect } from 'react';
 
-const modalRoot = document.getElementById("modal-root");
+const modalRoot = document.getElementById('modal-root');
 
 export default function FinishModal({ onClose }) {
-    
-    useEffect(() => {
-        const handleEsc = e => {
-          if (e.key === "Escape") {
-            onClose();
-          }
-        };
-    
-        window.addEventListener("keydown", handleEsc);
-        return () => window.removeEventListener("keydown", handleEsc);
-      }, [onClose]);
+  useEffect(() => {
+    const handleEsc = e => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
 
-  return createPortal (
-    <div className={styles.overlay} onClick={onClose}> 
-      <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
-        <button onClick={onClose}>X</button>
-        <h2>The book is read</h2>
-        <p>
-          It was an exciting journey, where each page revealed new horizons,
-          and the characters became inseparable friends.
+    window.addEventListener('keydown', handleEsc);
+    return () => window.removeEventListener('keydown', handleEsc);
+  }, [onClose]);
+
+  return createPortal(
+    <div className={styles.backdrop} onClick={onClose}>
+      <div className={styles.modalContent} onClick={e => e.stopPropagation()}>
+        <button onClick={onClose}>
+          <svg className={styles.closeIcon} width="22" height="22">
+            <use href="/icons.svg#icon-x" />
+          </svg>
+        </button>
+        <img
+          height="50"
+          width="50"
+          src="/books.png"
+          alt="books"
+          className={styles.img}
+        />
+        <h2 className={styles.title}>The book is read</h2>
+        <p className={styles.text}>
+          It was an <span className={styles.span}>exciting journey</span> ,
+          where each page revealed new horizons, and the characters became
+          inseparable <br />
+          friends.
         </p>
-          </div>
-      </div>,
-      modalRoot
-    );
+      </div>
+    </div>,
+    modalRoot
+  );
 }
